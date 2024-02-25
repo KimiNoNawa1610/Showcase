4bda9732fb3c951b1098cecf86002f2d28340b14
Nhan Vo <48186215+KimiNoNawa1610@users.noreply.github.com>
2024-02-25 07:33:51 -0800

    update footer holder

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -10,7 +10,20 @@ const Layout = ({ children }) => {
       <div className="content">{children}</div>
       <br/>
       <div className='contacts'>
-        this is contacts
+        <div className="ft-list" id="contact">
+            <p className="ft-list-title">HOANG NGO CONSTRUCTION COMPANY LIMITED</p>
+            <ul className="ft-list-items">
+              <li>
+                <a href="mailto:Connect.hnstudio@gmail.com">Connect.hnstudio@gmail.com</a>
+              </li>
+              <li>
+                <a href="tel:+84975409010">Hotline: 0975409010 (Mr.Thắng)</a>
+              </li>
+              <li>
+                <a href="tel:+84939959195">0939959195 (Mrs. Anh)</a>
+              </li>
+            </ul>
+          </div>
       </div>
     </div>
   );
